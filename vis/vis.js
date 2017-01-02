2bb32c36ab77b54c5ff61b8b2a98f0e94519850a
Chris Hayen <chris.hayen@performancehorizon.com>
2017-01-02 15:48:12 -0500

    three.js

--- vis/vis.js
+++ vis/vis.js
@@ -5,14 +5,4 @@ $(document).ready(function() {
 var data = "this is a test".split("")
 
 function makeSomething() {
-  var vis = d3.select("#vis")
-
-  var poly = d3.select(".hex")
-  poly.attr("fill", "#666")
-
-  var polyName = poly.property("nodeName");
-
-  vis.append(polyName)
-  .attr("fill", "#00ff00")
-  .attr("x", 100)
 }
