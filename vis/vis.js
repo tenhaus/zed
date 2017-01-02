2a1dc6624ede5515943c1f95cc60f11a626820c1
Chris Hayen <chris.hayen@performancehorizon.com>
2017-01-02 16:52:46 -0500

    Got text working

--- vis/vis.js
+++ vis/vis.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
-  init()
-  cubes()
-  render()
+  var loader = new THREE.FontLoader();
+	loader.load( 'fonts/helvetiker_regular.typeface.json', function ( response ) {
+		font = response
+    init()
+    cubes()
+    text()
+    render()
+	});
 })
 
 var data = "this is a test".split("")
 var scene, camera, renderer, root;
 var mouseX = 0, mouseY = 0;
+
+var font;
 
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
@@ -23,17 +30,18 @@ function init() {
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 }
 
+
 function onDocumentMouseMove(event) {
 	mouseX = (event.clientX - windowHalfX) * 10;
 	mouseY = (event.clientY - windowHalfY) * 10;
-  console.log(mouseX, mouseY)
 }
 
-function render() {
-  camera.position.x += ( mouseX - camera.position.x ) * .05;
-	camera.position.y += ( - mouseY - camera.position.y ) * .05;
 
-  camera.lookAt( scene.position );
+function render() {
+  // camera.position.x += ( mouseX - camera.position.x ) * .05;
+	// camera.position.y += ( - mouseY - camera.position.y ) * .05;
+
+  // camera.lookAt( scene.position );
 
   requestAnimationFrame(render);
   renderer.localClippingEnabled = false;
@@ -42,6 +50,29 @@ function render() {
 					Empty = Object.freeze( [] );
 				renderer.clippingPlanes = Empty;
   renderer.render(scene, camera);
+}
+
+function text() {
+  var textGeo = new THREE.TextGeometry( "heeeey", {
+		font: font,
+		size: 80,
+		height: 20,
+		curveSegments: 2
+	});
+
+  textGeo.computeBoundingBox();
+	textGeo.computeVertexNormals();
+
+  var material = new THREE.MeshBasicMaterial({color:0x00ffff})
+
+  var centerOffset = -0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+	textMesh1 = new THREE.Mesh( textGeo, material );
+	textMesh1.position.x = 150;
+	textMesh1.position.y = 150;
+	textMesh1.position.z = 0;
+	textMesh1.rotation.x = 0;
+	textMesh1.rotation.y = Math.PI * 2;
+	scene.add( textMesh1 );
 }
 
 function cubes() {
