ce93157a6351275df3e68a5499e19e22aa2dcf2f
Chris Hayen <chris.hayen@performancehorizon.com>
2017-01-02 15:55:39 -0500

    three.js is awesome

--- vis/vis.js
+++ vis/vis.js
@@ -1,8 +1,31 @@
 $(document).ready(function() {
-  makeSomething()
+  init()
+  cube()
+  render()
 })
 
 var data = "this is a test".split("")
+var scene, camera, renderer;
 
-function makeSomething() {
+function init() {
+  scene = new THREE.Scene();
+  camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+}
+
+function render() {
+  requestAnimationFrame(render);
+  renderer.render(scene, camera);
+}
+
+function cube() {
+  var geometry = new THREE.BoxGeometry(1,1,1)
+  var material = new THREE.MeshBasicMaterial({color:0x00ff00})
+  var cube = new THREE.Mesh(geometry, material)
+  scene.add(cube)
+
+  camera.position.z = 5;
 }
