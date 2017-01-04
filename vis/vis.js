e02081717b400212ca1bf0ff75cb03278f79321b
Chris Hayen <chris.hayen@performancehorizon.com>
2017-01-03 22:53:42 -0500

    stats and real plan a rotations

--- vis/vis.js
+++ vis/vis.js
@@ -3,6 +3,7 @@ $(document).ready(function() {
 	loader.load( 'fonts/helvetiker_regular.typeface.json', function ( response ) {
 		font = response
     init()
+    info()
     cubes()
     render()
 	});
@@ -18,31 +19,46 @@ var font;
 var windowHalfX = window.innerWidth / 2;
 var windowHalfY = window.innerHeight / 2;
 
+var rotationFrameCount = 0;
+
 function init() {
   scene = new THREE.Scene();
   camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
   camera.position.z = 800;
+  camera.position.x = windowHalfX*3;
   // camera.position.y = 500
 
   renderer = new THREE.WebGLRenderer();
   renderer.setSize(window.innerWidth, window.innerHeight);
-  document.body.appendChild(renderer.domElement);
-
+  document.getElementById("scene").appendChild(renderer.domElement);
   document.addEventListener( 'mousemove', onDocumentMouseMove, false );
 }
 
-
-function onDocumentMouseMove(event) {
-	mouseX = (event.clientX - windowHalfX) * 10;
-	mouseY = (event.clientY - windowHalfY) * 10;
-}
-
-
 function render() {
-  // camera.position.x += ( mouseX - camera.position.x ) * .05;
-	// camera.position.y += ( - mouseY - camera.position.y ) * .05;
+  // camera.position.x += ( mouseX - camera.position.x ) * .003;
+	// camera.position.y += ( - mouseY - camera.position.y ) * .003;
   // root.rotation.x += 0.1
-  camera.lookAt( scene.position );
+
+  rotationFrameCount++;
+
+  if (rotationFrameCount >= 10) {
+    var odd = -1;
+
+    scene.traverse(function(child) {
+      if(child.name == "oct") {
+        child.rotation.z += Math.PI / 3
+      }
+
+      if(child.name == "odd") {
+        child.rotation.z += (Math.PI / 3)*-1
+      }
+
+      // if (child.rotation.z == 360) child.rotation.z = 0
+      rotationFrameCount = 0;
+    });
+  }
+
+  // camera.lookAt( scene.position );
 
   requestAnimationFrame(render);
   renderer.localClippingEnabled = false;
@@ -75,6 +91,7 @@ function cubes() {
   var size = 80
   var geometry = new THREE.TorusGeometry(size, 2.5, 6, 0)
   var material = new THREE.MeshBasicMaterial({color:0x00ff00, wireframe: true})
+  var oddMaterial = new THREE.MeshBasicMaterial({color:0xcccccc, wireframe: true})
 
   root = new THREE.Mesh( geometry, material);
 	root.position.x = windowHalfX * -1;
@@ -92,12 +109,26 @@ function cubes() {
 
   var amount = 200, object, parent = root;
 
-  for(var i = 6; i < data.length; i ++) {
-		object = new THREE.Mesh(geometry, material);
-		object.position.x = 150;
-    // object.rotation.x = 25
-		parent.add(object);
-		parent = object;
+  var odd = false;
+
+  for(var i = 6; i < data.length/6; i ++) {
+		var object;
+
+    if(odd) {
+      object = new THREE.Mesh(geometry, oddMaterial);
+      object.name = "odd"
+      object.rotation.z = (Math.PI / 3)*-1
+    }
+    else {
+      object = new THREE.Mesh(geometry, material);
+      object.name = "oct"
+      object.rotation.z = Math.PI / 3
+    }
+
+    odd = !odd;
+
+    object.position.x = i*150;
+    scene.add(object)
 
     text(data[i], object, hs/2, hs)
     text(data[i+1], object, hs, 0)
@@ -106,22 +137,17 @@ function cubes() {
     text(data[i+4], object, hs*-1, 0)
     text(data[i+5], object, (hs/2)*-1, hs)
 	}
+}
 
-  // parent = root
-  //
-  // for(var i = 0; i < amount; i ++) {
-	// 	object = new THREE.Mesh(geometry, material);
-	// 	object.position.x = -150;
-  //   object.rotation.x = -25
-	// 	parent.add(object);
-	// 	parent = object;
-  //
-  //   text("a", object, hs, 0)
-  //   text("b", object, hs/2, hs*-1)
-  //   text("c", object, (hs/2)*-1, hs*-1)
-  //   text("d", object, hs*-1, 0)
-  //   text("e", object, (hs/2)*-1, hs)
-  //   text("f", object, hs/2, hs)
-	// }
+function onDocumentMouseMove(event) {
+	mouseX = (event.clientX - windowHalfX) * 10;
+	mouseY = (event.clientY - windowHalfY) * 10;
+}
 
+function info() {
+  var p = document.createElement("P");                       // Create a <p> node
+  var t = document.createTextNode("Bytes " + data.length);      // Create a text node
+  p.appendChild(t);
+
+  document.getElementById("stats").appendChild(p)
 }
